aa5265d01c21707afcf69dec75b171c77bca1501
JBazinIPS2 <57449528+JBazinIPS2@users.noreply.github.com>
2019-11-14 10:16:16 +0100

    Update and rename suppa.sh to SUPPA_analysis.sh

--- SUPPA_analysis.sh
+++ SUPPA_analysis.sh
@@ -1,8 +1,19 @@
-/usr/bin/bash
+#!/usr/bin/bash
 
-SUPPA analysis gene of AS
+SUPPA analysis of AS
 ============================================
 
+### generate events from annotation
+python3 ~/.local/lib/python3.4/site-packages/SUPPA/suppa.py generateEvents -i AtRTD2_QUASI.gtf -e SE SS RI FL MX -o AtRTD2_all_events.ioe -f ioe
+
+## make tx tpm dataframe from kallisto counts 
+python3 ~/.local/lib/python3.4/site-packages/SUPPA/multipleFieldSelection.py -i in/kallisto/*/abundance.tsv -k 1 -f 5 -o out/iso_all_sample.tpm.txt
+
+## calculate psiPerEvent
+python3 ~/.local/lib/python3.4/site-packages/SUPPA/suppa.py psiPerEvent -i in/AtRTD2_all_events.ioe -e out/iso_all_sample.tpm.txt -o out/psy_allsamples
+
+
+### differential splicing analysis
 
   A=OE_ASCO
   B=Col
